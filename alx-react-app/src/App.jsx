0a3f5c93680ffdfed18e616ef533b08c5524c618
Shimelis Teshome <shimelisteshome1996@gmail.com>
2025-07-09 19:42:08 +0300

    application of reacts part with header, main amd footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,11 @@
+
+
+import WelcomeMessage from './components/WelcomeMessage'
+
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 
-import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -27,7 +30,7 @@ function App() {
 
         <WelcomeMessage />
         <Header />
-        <MainContent />
+        <MainContent/>
         <Footer />
 
       <h1>Vite + React</h1>
