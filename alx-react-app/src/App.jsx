dcbe9a67941a043fa86d280100d9e22c2595f9c3
Shimelis Teshome <shimelisteshome1996@gmail.com>
2025-07-09 19:07:12 +0300

    Add welcomeMessage component and integrate in to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -16,6 +17,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
+      {/* the inserted WelcomeMessage component
+      N.b div is optional */}
+
+
+      <div>
+        <WelcomeMessage/>
+      </div>
+
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
