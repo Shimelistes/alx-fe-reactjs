279b130c9da881ea6ba6f02a7de9bed9591c7721
Shimelis Teshome <shimelisteshome1996@gmail.com>
2025-07-18 13:55:15 +0300

    Apply inline styling to react

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header () {
   return (
     <header style={{backgroudcolor: 'navy', color: 'white', 
-      textAlign:'center'
+      textAlign:'center', border: '1px solid yellow'
     }}>
       <h1>"My Favorite Cities"</h1>
     </header>
