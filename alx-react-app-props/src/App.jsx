b0cc72fe97c541acbe87e5ab79e2a717abcf8c1c
Shimelis Teshome <shimelisteshome1996@gmail.com>
2025-07-19 13:06:31 +0300

    Final modified file

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+
 import ProfilePage from './components/ProfilePage';
-import UserContext from './UserContext'; 
+import UserContext from './UserContext';
+
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
+    <div>
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+
+      <div>
+        <a href="https://vite.dev " target="_blank" rel="noopener noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev " target="_blank" rel="noopener noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      {/* Uncomment and use these components as needed */}
+      <div>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
+
+        <UserProfile 
+          name="Alice" 
+          age="25" 
+          bio="Loves hiking and photography" 
+        />
+      </div>
+    </div>
   );
 }
 
