88854bccbbe39c1820753e888aff64f916e5875d
Shimelis Teshome <shimelisteshome1996@gmail.com>
2025-07-26 23:17:23 +0300

    update Recipe Sharing Application

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RecipeDetails from './components/RecipeDetails'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/recipe/:id',
+    element: <RecipeDetails />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,9 +1,30 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
-}))
+  
+  // Add a new recipe
+  addRecipe: (newRecipe) => 
+    set((state) => ({ 
+      recipes: [...state.recipes, { ...newRecipe, id: Date.now() }] 
+    })),
+  
+  // Delete a recipe by ID
+  deleteRecipe: (id) => 
+    set((state) => ({ 
+      recipes: state.recipes.filter(recipe => recipe.id !== id) 
+    })),
+  
+  // Update an existing recipe
+  updateRecipe: (id, updatedRecipe) => 
+    set((state) => ({
+      recipes: state.recipes.map(recipe => 
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      )
+    })),
+  
+  // Initialize recipes (optional)
+  setRecipes: (recipes) => set({ recipes })
+}));
 
-export default useRecipeStore
+export default useRecipeStore;
