a4c667e9efd97cb03d87292de7b8627d20971d4b
Shimelis Teshome <shimelisteshome1996@gmail.com>
2025-08-17 13:27:14 +0300

    Setting up react files for recipe-sharing-platform

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,12 +1,10 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
 
