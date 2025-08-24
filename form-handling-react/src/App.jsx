9a69c0dd554661d06207db4c2ab4e75a469c0fe7
Shimelis Teshome <shimelisteshome1996@gmail.com>
2025-08-24 12:51:51 +0300

    formikForm updated Files

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,13 +1,16 @@
-import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/formikForm";
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
 
-function App() {
+export default function App() {
   return (
-    <div className="p-6 space-y-10">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4 font-sans">
+      {/* Render the controlled registration form */}
       <RegistrationForm />
-      <formikForm />
+
+      <div className="my-8 text-gray-600 text-lg font-medium">--- OR ---</div>
+
+      {/* Render the Formik registration form */}
+      <FormikRegistrationForm />
     </div>
   );
 }
-
-export default App;
